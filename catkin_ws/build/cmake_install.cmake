bf3a5a76d4be4a6793409a67e9aa4de6a81eeff0
DarkKnight <daniel.lvxin@gmail.com>
2019-01-29 17:29:55 +0800

    create learning communication package
    Signed-off-by: DarkKnight <daniel.lvxin@gmail.com>

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/amit/ROS/ROS-deepblue-study/ROS-theory-practice/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/amit/ROS/ROS-deepblue-study/ROS-theory-practice/catkin_ws/build/learning_communication/cmake_install.cmake")
 
 endif()
 
---
+++ catkin_ws/build/learning_communication/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/amit/ROS/ROS-deepblue-study/ROS-theory-practice/catkin_ws/src/learning_communication
+# Build directory: /home/amit/ROS/ROS-deepblue-study/ROS-theory-practice/catkin_ws/build/learning_communication
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
