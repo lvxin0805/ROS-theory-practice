3858f95a5fd1d503d70ee95a44dd57317228049d
DarkKnight <daniel.lvxin@gmail.com>
2019-01-29 16:43:08 +0800

    workspace initialization
    Signed-off-by: DarkKnight <daniel.lvxin@gmail.com>

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/amit/ROS/ROS-deepblue-study/ROS-theory-practice/catkin_ws/src
+# Build directory: /home/amit/ROS/ROS-deepblue-study/ROS-theory-practice/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ catkin_ws/build/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/amit/ROS/ROS-deepblue-study/ROS-theory-practice/catkin_ws/build/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
